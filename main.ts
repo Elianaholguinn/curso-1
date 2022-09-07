7fabeb3eb922bb033f4f4665c9f8cb92afbc7f9d
Elianaholguinn <110990969+Elianaholguinn@users.noreply.github.com>
2022-09-06 20:38:18 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+})
